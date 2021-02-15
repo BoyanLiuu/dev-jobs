1d398db19535a7f6c9aa8a4eddccd0a60d661763
Minhui Xie <MXie@cese.fraunhofer.org>
2021-02-14 19:00:09 -0500

    initialized firebase DB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  };
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
     <React.StrictMode>
